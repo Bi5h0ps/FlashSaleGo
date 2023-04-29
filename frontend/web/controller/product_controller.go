7ef1d091d285402854877ec93bdc8d39922fb24f
Theo <h62guo@edu.uwaterloo.ca>
2023-04-29 20:27:06 +0800

    minor changes to the error handling

--- frontend/web/controller/product_controller.go
+++ frontend/web/controller/product_controller.go
@@ -17,12 +17,16 @@ type ProductController struct {
 func (p *ProductController) GetDetail(c *gin.Context) {
 	id, err := strconv.ParseInt(c.Query("productID"), 10, 64)
 	if err != nil {
-		c.Error(err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"err": "productID provided in wrong format",
+		})
 		return
 	}
 	product, err := p.ProductService.GetProductById(id)
 	if err != nil {
-		c.Error(err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"err": "productID incorrect, no such item found",
+		})
 		return
 	}
 	c.HTML(http.StatusOK, "detail", gin.H{
